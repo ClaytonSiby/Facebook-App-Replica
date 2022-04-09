975492133db6126babb8b398a588128b0d6d5318
ClaytonSiby <claytonsiby@gmail.com>
2022-04-09 20:30:44 +0200

    Setup authentication on the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    # run these methods before every other controller action.
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name image])
+      devise_parameter_sanitizer.permit(:account_updatek, keys: %i[first_name last_name image])
+    end
 end
