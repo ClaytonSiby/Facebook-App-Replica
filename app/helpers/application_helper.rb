9e268e7b468024720a1e35eb7a2b208c168520aa
ClaytonSiby <claytonsiby@gmail.com>
2022-04-10 09:39:25 +0200

    Add the friendship helper methods in the ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,25 @@ module ApplicationHelper
 
     result
   end
+
+  def friend_request_sent?(user)
+    current_user.friend_sent.exists?(sent_to_id: user.id, status: false)
+  end
+
+  def friend_request_received?(user)
+    current_user.friend_request.exists?(sent_by_id: user.id, status: false)
+  end
+
+  # check whether a user has had a friend request sent to them by the current user or
+  # if the current user has been sent a friend request by the user returning either true or false
+
+  def possible_friend?(user)
+    request_sent = current_user.friend_sent.exists?(sent_to_id: user.id)
+
+    request_received = current_user.friend_request.exists?(sent_by_id: user.id)
+
+    true if request_sent != request_received
+    true if request_sent == request_received && request_sent == true
+    false if request_sent == request_received && request_sent == false
+  end
 end
