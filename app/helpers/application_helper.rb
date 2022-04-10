835d88ed0912b9d633c974974000b22779ee785a
ClaytonSiby <claytonsiby@gmail.com>
2022-04-10 09:19:45 +0200

    Add the like helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,35 @@
 module ApplicationHelper
-    #  returns the new record created in notifications table
-    def new_notification(user, notice_id, notice_type)
-      notice = user.notifications.build(notice_id: notice_id, notice_type: notice_type)
+  #  returns the new record created in notifications table
+  def new_notification(user, notice_id, notice_type)
+    notice = user.notifications.build(notice_id: notice_id, notice_type: notice_type)
 
-      user.notice_seen = false
-      user.save
-      notice
-    end
+    user.notice_seen = false
+    user.save
+    notice
+  end
 
-    # receives the notification object as parameter along with a type and
-    #  returns a User record, Post record or a Comment record
-    #  depending on the type supplied
+  # receives the notification object as parameter along with a type and
+  #  returns a User record, Post record or a Comment record
+  #  depending on the type supplied
 
-    def notification_find(notice, type)
-      User.find(notice.notice_id) if type == 'friendRequest'
-      Post.find(notice.notice_id) if type == 'comment'
-      Post.find(notice.notice_id) if type == 'like-post'
+  def notification_find(notice, type)
+    User.find(notice.notice_id) if type == 'friendRequest'
+    Post.find(notice.notice_id) if type == 'comment'
+    Post.find(notice.notice_id) if type == 'like-post'
 
-      return unless type == 'like-comment'
+    return unless type == 'like-comment'
 
-      comment = Comment.find(notice.notice_id)
+    comment = Comment.find(notice.notice_id)
 
-      Post.find(comment.post_id)
-    end
+    Post.find(comment.post_id)
+  end
+
+  def liked?(subject, type)
+    result = false
+
+    result = Like.where(user_id: current_user.id, post_id: subject.id).exists if type == 'post'
+    result = Like.where(user_id: current_user.id, comment_id: subject.id).exists if type == 'comment'
+
+    result
+  end
 end
